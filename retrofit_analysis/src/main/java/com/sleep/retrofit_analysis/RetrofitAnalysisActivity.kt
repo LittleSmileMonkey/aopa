11c39ebab684ddc160fc7c1339b902b09afa4a56
sleep <sleepym09@163.com>
2019-05-21 13:02:26 +0800

    add retrofit and okhttp dependence

---
+++ retrofit_analysis/src/main/java/com/sleep/retrofit_analysis/RetrofitAnalysisActivity.kt
@@ -0,0 +1,12 @@
+package com.sleep.retrofit_analysis
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RetrofitAnalysisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit_analysis)
+    }
+}
